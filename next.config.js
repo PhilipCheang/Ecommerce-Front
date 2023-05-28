4c965888a9c584e8e6a2eb6f3d62b679f810689c
[Philip Cheang] <[philipcheang7@gmail.com]>
2023-05-28 09:18:29 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-const nextConfig = {
+// next.config.js
+const withImages = require('next-images');
+
+module.exports = withImages({
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
   images: {
     domains: ['philip-next-ecommerce.s3.us-east-2.amazonaws.com', 'philip-next-ecommerce.s3.amazonaws.com'],
   },
-};
+});
 
-module.exports = nextConfig;
+
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+//   images: {
+//     domains: ['philip-next-ecommerce.s3.us-east-2.amazonaws.com', 'philip-next-ecommerce.s3.amazonaws.com'],
+//   },
+// };
+
+// module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,36 @@
-// pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+// // pages/_document.js
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-
-// import { Html, Head, Main, NextScript } from 'next/document'
-
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head />
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   )
+// export default class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html lang="en">
+//         <Head>
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+//             rel="stylesheet"
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
 // }
+
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
